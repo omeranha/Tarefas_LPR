2a65d2864a07b43cf6c463de90684b513bd05d69
omeranha <53233626+omeranha@users.noreply.github.com>
2024-10-24 14:58:27 -0300

    aula11

---
+++ aula11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO {
+	internal class Program {
+		static void Main(string[] args)
+		{
+			Retangulo retangulo = new();
+			Console.WriteLine("Insira a altura e a largura do retangulo, respectivamente");
+			retangulo.altura = double.Parse(Console.ReadLine());
+			retangulo.largura = double.Parse(Console.ReadLine());
+			Console.WriteLine("Area do retangulo: {0} perimetro do retangulo: {1}", retangulo.calcArea(), retangulo.calcPerimetro());
+			Console.ReadLine();
+		}
+	}
+}
---
+++ aula11/Retangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO {
+	internal class Retangulo {
+		public double altura;
+		public double largura;
+
+		public double calcArea()
+		{
+			return (altura * largura);
+		}
+
+		public double calcPerimetro()
+		{
+			return (2 * (altura + largura));
+		}	
+	}
+}
